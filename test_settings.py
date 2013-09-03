07fc772c16c151caffdd4a6e656538453fc339e4
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-09-03 13:40:23 +0200

    Graceously handle South, adminsortable in test_settings.

--- test_settings.py
+++ test_settings.py
@@ -10,11 +10,20 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'portfolio.context_processors.categories'
 )
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'sorl.thumbnail',
+    'adminsortable',
     'portfolio'
-)
+]
+
+try:
+    # If available, South is required by setuptest
+    import south
+    INSTALLED_APPS.append('south')
+except ImportError:
+    # South not installed and hence is not required
+    pass
 
 ROOT_URLCONF = 'test_urls'
 
